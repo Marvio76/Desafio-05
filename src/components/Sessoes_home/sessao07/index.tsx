9bbf1c2fc7de3d70b00f4a7fad8d9c306c376207
I4ze <Iaze.guilherme25@gmail.com>
2025-06-28 21:16:46 -0300

    Estilo: adiciona responsividade sessao 04 e 07

--- src/components/Sessoes_home/sessao07/index.tsx
+++ src/components/Sessoes_home/sessao07/index.tsx
@@ -1,46 +1,43 @@
 import style from './styles.module.scss';
-import bandeiraMaranhao from '../../../assets/images/sessao07/Bandeira_do_Maranhão.png'
-import fapema from '../../../assets/images/sessao07/fapema.png'
-import linkedin from '../../../assets/images/sessao07/linkedin-logo.svg'
+import bandeiraMaranhao from '../../../assets/images/sessao07/Bandeira_do_Maranhão.png';
+import fapema from '../../../assets/images/sessao07/fapema.png';
+import linkedin from '../../../assets/images/sessao07/linkedin-logo.svg';
 
 interface Sessao07Props {}
 
 const Sessao07: React.FC<Sessao07Props> = () => {
     return (
         <div className={style.container}>
-            <div className={style.row}>
-                <div className={style.leftColumn}>
-                    <div className={style.infoBox}>
-                        <div className={style.titleGroup}>
-                            <div className={style.titleWithIcon}>
-                                <img className={style.iconLarge} src={bandeiraMaranhao} alt=''/>
-                                <div className={style.title}>Telemedicina</div>
-                            </div>
-                            <div className={style.subtitle}>
-                                Sua saúde é a nossa especialidade, e sua satisfação é o nosso desejo
-                            </div>
+            <div className={style.content}>
+                <div className={style.infoSection}>
+                    <div className={style.titleGroup}>
+                        <div className={style.titleWithIcon}>
+                            <img className={style.iconLarge} src={bandeiraMaranhao} alt="Bandeira do Maranhão"/>
+                            <h2 className={style.title}>Telemedicina</h2>
                         </div>
-
-                        <div className={style.linkSection}>
-                            <div className={style.linkText}>Link da nossa equipe:</div>
-                            <div className={style.linkBox}>
-                                {/* <div className={style.iconPlaceholder} />
-                                <div className={style.iconBackground} /> */}
-                                <img src={linkedin} alt="" />
-                            </div>
-                        </div>
+                        <p className={style.subtitle}>
+                            Sua saúde é a nossa especialidade, e sua satisfação é o nosso desejo
+                        </p>
                     </div>
-                    <img className={style.bannerImage} src={fapema}alt='' />
+
+                    <div className={style.linkSection}>
+                        <p className={style.linkText}>Link da nossa equipe:</p>
+                        <a href="#" className={style.linkBox} aria-label="LinkedIn">
+                            <img src={linkedin} alt="LinkedIn" />
+                        </a>
+                    </div>
+                    <img className={style.bannerImage} src={fapema} alt="Logo FAPEMA" />
                 </div>
 
-                <div className={style.servicosTitle}>Serviços:</div>
-
-                <div className={style.servicosList}>
-                    <div className={style.servico}>Psicologo</div>
-                    <div className={style.servico}>Nutricionista</div>
-                    <div className={style.servico}>Pediatra</div>
-                    <div className={style.servico}>Geriatra</div>
-                    <div className={style.servico}>Ortopedista</div>
+                <div className={style.servicesSection}>
+                    <h3 className={style.servicosTitle}>Serviços:</h3>
+                    <ul className={style.servicosList}>
+                        <li className={style.servico}>Psicologo</li>
+                        <li className={style.servico}>Nutricionista</li>
+                        <li className={style.servico}>Pediatra</li>
+                        <li className={style.servico}>Geriatra</li>
+                        <li className={style.servico}>Ortopedista</li>
+                    </ul>
                 </div>
             </div>
         </div>
